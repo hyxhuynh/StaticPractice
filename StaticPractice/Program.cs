b7b07d60fb1ae3730227fccf7279d927bd4f9493
Hy Huynh <hyxhuynh.projects@gmail.com>
2024-04-08 09:48:36 -0700

    initial commit

---
+++ StaticPractice/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace StaticPractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Fahrenheit temperature:");
+            double fahrenheitInput = double.Parse(Console.ReadLine());
+            double celsiusResult = Math.Round(TempConverter.FahrenheitToCelsius(fahrenheitInput), 3);
+            Console.WriteLine($"{fahrenheitInput} Fahrenheit equals {celsiusResult} Celsius");
+
+            Console.WriteLine("Enter Celsius temperature:");
+            double celsiusInput = double.Parse(Console.ReadLine());
+            double fahrenheitResult = Math.Round(TempConverter.CelsiusToFahrenheit(celsiusInput), 3);
+            Console.WriteLine($"{celsiusInput} Celsius equals {fahrenheitResult} Fahrenheit");
+        }
+    }
+}
---
+++ StaticPractice/TempConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticPractice
+{
+    public static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double fahrenheitTemp)
+        {
+            double celsiusTemp = (fahrenheitTemp - 32) * 5 / 9;
+            return celsiusTemp;
+        }
+        public static double CelsiusToFahrenheit(double celsiusTemp)
+        {
+            double farenheitTemp = (celsiusTemp * 9) / 5 + 32;
+            return farenheitTemp;
+        }
+    }
+}
